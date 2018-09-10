f5ff924ffa2ceccff3628d1c26a7a78b1a73db93
Steve Richards <20487107+steveeri@users.noreply.github.com>
2018-09-10 15:08:29 +1000

    Update format of code

--- userinput.cpp
+++ userinput.cpp
@@ -4,31 +4,32 @@
 // Created on  : 10/09/18.
 // Version     : 0.1a
 // Copyright   : TBA
-// Description : Reads the unbuffered keyboard inputs.
+// Description : Reads the unbuffered keyboard inputs from the terminal. [ Linux ]
 //============================================================================
 
 #include <iostream>
 #include <unistd.h>
 #include <termios.h>
 
+// Read character from keypresses. 
+// Toggle Echo & Canonical modes before and after read to to supress character display.
 char userInput() {
-        char buf = 0;
-	
-        struct termios old = {0};
-	
-        if (tcgetattr(0, &old) < 0)
-                perror("tcsetattr()");
-        old.c_lflag &= ~ICANON;
-        old.c_lflag &= ~ECHO;
-        old.c_cc[VMIN] = 1;
-        old.c_cc[VTIME] = 0;
-        if (tcsetattr(0, TCSANOW, &old) < 0)
-                perror("tcsetattr ICANON");
-        if (read(0, &buf, 1) < 0)
-                perror ("read()");
-        old.c_lflag |= ICANON;
-        old.c_lflag |= ECHO;
-        if (tcsetattr(0, TCSADRAIN, &old) < 0)
-                perror ("tcsetattr ~ICANON");
-        return (buf);
+
+	// Set terminal attributes for Nil Echo & Canonical.
+    struct termios tmAttrs = {0};	
+    if (tcgetattr(0, &tmAttrs) < 0) perror("tcsetattr()");
+    tmAttrs.c_lflag &= ~ICANON;
+    tmAttrs.c_lflag &= ~ECHO;
+    tmAttrs.c_cc[VMIN] = 1;
+    tmAttrs.c_cc[VTIME] = 0;
+    if (tcsetattr(0, TCSANOW, &tmAttrs) < 0) perror("tcsetattr ICANON");
+
+	// Read keyboard input buffer. Restore Nil Echo & Canonical.
+    char buf = 0;
+	if (read(0, &buf, 1) < 0) perror("read()");
+    tmAttrs.c_lflag |= ICANON;
+    tmAttrs.c_lflag |= ECHO;
+    if (tcsetattr(0, TCSADRAIN, &tmAttrs) < 0) perror("tcsetattr ~ICANON");
+
+	return (buf); // return input.
 }
